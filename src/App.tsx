aefeb2fed762ef8608fcb736af5f75832c6e58f2
TariqNew <marandumashaka78@gmail.com>
2025-05-19 18:39:42 +0300

    chore: Adding qr code to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Accordian from "./components/accordian/Accordian";
 import LoadData from "./components/Load_Data/LoadData";
 import Slider from "./components/imageSlider/Slider";
+import QrCode from "./components/Qr-code/QrCode";
+import Dashboard from "./components/dashboard/Dashboard";
 
 export default function App(){
   return (
@@ -8,7 +10,8 @@ export default function App(){
       <Accordian />
       <Slider url="https://picsum.photos/v2/list" limit = {5} />
       <LoadData urlData="https://www.themealdb.com/api/json/v1/1/search.php?s=" />
-
+      <Dashboard />
+      <QrCode />
     </div>
   )
 }
